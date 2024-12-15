706abafe5defaa59a748a7557c7e7cb08d866bc1
Jesper Lundgren <koudelkaa@gmail.com>
2024-12-15 21:38:16 +0800

    correct version in mix.exs

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExScylla.MixProject do
   def project do
     [
       app: :ex_scylla,
-      version: "0.5.0",
+      version: "0.5.1",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: LcovEx, output: "cover"],
